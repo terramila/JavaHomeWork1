7d3292dc701123eadbefbce1412aabc419101ce6
tarana <beshirova@hotmail.com>
2023-01-26 13:28:13 +0300

    task1

---
+++ task1.java
@@ -0,0 +1,20 @@
+// Написать программу вычисления n-ого треугольного числа. 
+package seminar1;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.print("Введите число : ");
+        int a = Scanner.nextInt();
+        Scanner.close();
+        System.out.println("Треугольное число = " + Triangle(a) + "\n");
+    }
+
+    public static int Triangle(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return Triangle(num - 1) + num;
+    }
+}
